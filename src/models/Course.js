a190fd9946ff03aa31d97766aad6202568b2f560
z78hd82thx-ui <z78hd82thx@privaterelay.appleid.com>
2026-03-13 13:04:16 +0100

    Create Course.js file with Course schema for MongoDB

---
+++ src/models/Course.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
